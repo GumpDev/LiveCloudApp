ad6ce5a57eea07f4a447e8014bccd161ffa60885
GumpFlash <gump.flash@gmail.com>
2018-06-19 12:20:26 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace LiveCloud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Sem Título-2.png");
+            this.imageList1.Images.SetKeyName(1, "folder.png");
+            this.imageList1.Images.SetKeyName(2, "foldercloud.png");
+            this.imageList1.Images.SetKeyName(3, "folderdfff.png");
+            this.imageList1.Images.SetKeyName(4, "file.png");
+            this.imageList1.Images.SetKeyName(5, "fileCloud.png");
+            this.imageList1.Images.SetKeyName(6, "fileDiferente.png");
+            this.imageList1.Images.SetKeyName(7, "folders.png");
+            this.imageList1.Images.SetKeyName(8, "files.png");
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(575, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(225, 450);
+            this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 33);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "(tamanho)";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "(nome)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Margin = new System.Windows.Forms.Padding(0);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(575, 450);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.SmallIcon;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 424);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(575, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "LiveCloud";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Net;
+
+namespace LiveCloud
+{
+    public partial class Form1 : Form
+    {
+        string path;
+        string defaultPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\LiveCloud\\";
+        string pathM;
+        string url = "http://localhost/LiveCloud";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            path = defaultPath;
+            if (!Directory.Exists(defaultPath))
+            {
+                Directory.CreateDirectory(defaultPath);
+            }
+            label1.Text = "";
+            label2.Text = "";
+            GetFilesInDirectory();
+        }
+
+        private void GetFilesInDirectory()
+        {
+            textBox1.Text = path;
+            listView1.Items.Clear();
+            if(path != defaultPath)
+            {
+                ListViewItem item = new ListViewItem();
+                item.ImageIndex = 0;
+                item.Text = "...";
+                listView1.Items.Add(item);
+            }
+            string[] folders = Directory.GetDirectories(path);
+            for (int i = 0; i < folders.Length; i++)
+            {
+                string name = folders[i].Split('\\')[folders[i].Split('\\').Length - 1];
+                string pathfl = folders[i];
+                ListViewItem item = new ListViewItem();
+                item.ImageIndex = 1;
+                item.Text = name;
+                listView1.Items.Add(item);
+            }
+            string[] files = Directory.GetFiles(path);
+            for (int i = 0; i < files.Length; i++)
+            {
+                string name = files[i].Split('\\')[files[i].Split('\\').Length - 1];
+                string pathfl = files[i];
+                ListViewItem item = new ListViewItem();
+                item.ImageIndex = 4;
+                item.Text = name;
+                listView1.Items.Add(item);
+            }
+            WebClient wc = new WebClient();
+            string wFiles = wc.DownloadString(url + "/getFiles.php?loc=" + pathM);
+            for(int i = 0; i < wFiles.Split('|').Length - 2; i++)
+            {
+                string name = wFiles.Split('|')[i];
+                i++;
+                int size = int.Parse(wFiles.Split('|')[i]);
+                int status = 0;
+                int id = 0;
+                for(int x = 0; x < listView1.Items.Count; x++)
+                {
+                    if(listView1.Items[x].Text == name)
+                    {
+                        MessageBox.Show(File.ReadAllBytes(path + listView1.Items[i].Text).Length.ToString());
+                        MessageBox.Show(size.ToString());
+                        if(size != File.ReadAllBytes(path + listView1.Items[i].Text).Length)
+                        {
+                            status = 1;
+                        }
+                        else
+                        {
+                            status = 2;
+                        }
+                    }
+                    id = x;
+                    break;
+                }
+                if(status == 0)
+                {
+                    string namen = name;
+                    ListViewItem item = new ListViewItem();
+                    if (size == 0)
+                        item.ImageIndex = 2;
+                    else
+                        item.ImageIndex = 5;
+                    item.Text = namen;
+                    listView1.Items.Add(item);
+                }
+                else if (status == 1)
+                {
+                    string namen = name;
+                    ListViewItem item = new ListViewItem();
+                    if (size == 0)
+                        item.ImageIndex = 3;
+                    else
+                        item.ImageIndex = 6;
+                    item.Text = namen;
+                    listView1.Items[id] = item;
+                }else if(status == 2)
+                {
+                    string namen = name;
+                    ListViewItem item = new ListViewItem();
+                    if (size == 0)
+                        item.ImageIndex = 7;
+                    else
+                        item.ImageIndex = 8;
+                    item.Text = namen;
+                    listView1.Items[id] = item;
+                }
+            }
+        }
+
+        static readonly string[] SizeSuffixes =
+                  { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+        static string SizeSuffix(Int64 value, int decimalPlaces = 1)
+        {
+            if (decimalPlaces < 0) { throw new ArgumentOutOfRangeException("decimalPlaces"); }
+            if (value < 0) { return "-" + SizeSuffix(-value); }
+            if (value == 0) { return string.Format("{0:n" + decimalPlaces + "} bytes", 0); }
+
+            int mag = (int)Math.Log(value, 1024);
+
+            decimal adjustedSize = (decimal)value / (1L << (mag * 10));
+
+            if (Math.Round(adjustedSize, decimalPlaces) >= 1000)
+            {
+                mag += 1;
+                adjustedSize /= 1024;
+            }
+
+            return string.Format("{0:n" + decimalPlaces + "} {1}",
+                adjustedSize,
+                SizeSuffixes[mag]);
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            if(listView1.SelectedItems.Count > 0)
+            {
+                string selected = listView1.SelectedItems[0].Text;
+                if(File.Exists(path + selected))
+                {
+                    Process.Start(path + selected);
+                }
+                else
+                {
+                    if (selected == "...")
+                    {
+                        string x = "";
+                        pathM = "";
+                        for(int i = 0; i < path.Split('\\').Length -2; i++)
+                        {
+                            x += path.Split('\\')[i] + "\\";
+                            if (i > 5)
+                                pathM += path.Split('\\')[i] + "\\";
+                        }
+                        path = x;
+                        GetFilesInDirectory();
+                    }
+                    else
+                    {
+                        path += selected + "\\";
+                        pathM += selected + "\\";
+                        GetFilesInDirectory();
+                    }
+                }
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string selected = listView1.SelectedItems[0].Text;
+                if (selected == "...")
+                {
+                    label1.Text = "...";
+                    label2.Text = "Back to last Dircctory";
+                }
+                else
+                {
+                    if(listView1.SelectedItems[0].ImageIndex == 2 || listView1.SelectedItems[0].ImageIndex == 5)
+                    {
+                        label1.Text = selected;
+                        label2.Text = "in Server";
+                    }else if (listView1.SelectedItems[0].ImageIndex == 6 || listView1.SelectedItems[0].ImageIndex == 3)
+                    {
+                        label1.Text = selected;
+                        label2.Text = "Different From Server";
+                    }
+                    else if (File.Exists(path + selected))
+                    {
+                        label1.Text = selected;
+                        label2.Text = "Size: " + SizeSuffix(File.ReadAllBytes(path + selected).Length);
+                    }
+                    else
+                    {
+                        label1.Text = selected;
+                        label2.Text = "Folder";
+                    }
+                }
+            }
+        }
+    }
+}
